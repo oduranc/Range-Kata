792a55c852027edd7679becfde5f9a2786942ac6
OSCAR JOSE DURAN CUELLO <odurancuello@gmail.com>
2022-06-15 19:51:13 -0400

    Adding GetAllPoints

--- main.py
+++ main.py
@@ -20,16 +20,51 @@ class Range:
         self.typeRight = typeRight
 
     # Integer range contains
-    def IntegerRangeContains(range1, numbers):
-        i = 0
-        flag = 0
+    def IntegerRangeContains(range1, numbertoCheck):
         if range1.typeLeft == LimitTypes.closeLimit:
             if range1.typeRight == LimitTypes.closeLimit:
-                for number in range(range1.valueLeft, range1.valueRight + 1):
-                    if number != numbers[i]:
-                        flag = flag + 1
-                        i = i + 1
+                if numbertoCheck in range(range1.valueLeft, range1.valueRight + 1):
+                    return True
+                else:
+                    return False
             else:
-                range(range1.valueLeft, range1.valueRight)
+                if numbertoCheck in range(range1.valueLeft, range1.valueRight):
+                    return True
+                else:
+                    return False
         else:
-            print()
+            if range1.typeRight == LimitTypes.closeLimit:
+                if numbertoCheck in range(range1.valueLeft + 1, range1.valueRight + 1):
+                    return True
+                else:
+                    return False
+            else:
+                if numbertoCheck in range(range1.valueLeft + 1, range1.valueRight):
+                    return True
+                else:
+                    return False
+
+    # Get All Points
+    def GetAllPoints(range1):
+        points = []
+        if range1.typeLeft == LimitTypes.closeLimit:
+            # Left Closed
+            if range1.typeRight == LimitTypes.closeLimit:
+                # Right Closed
+                for number in range(range1.valueLeft, range1.valueRight + 1):
+                    points.append(number)
+            else:
+                # Right Opened
+                for number in range(range1.valueLeft, range1.valueRight):
+                    points.append(number)
+        else:
+            # Left Opened
+            if range1.typeRight == LimitTypes.closeLimit:
+                # Right Closed
+                for number in range(range1.valueLeft + 1, range1.valueRight + 1):
+                    points.append(number)
+            else:
+                # Right Opened
+                for number in range(range1.valueLeft + 1, range1.valueRight):
+                    points.append(number)
+        return points
